a8d8177f50a30676cab2b08769b06881b9f0f854
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-25 22:37:24 +0530

    Add Theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          blue: 'hsl(230,35%,7%)',
+          violet: 'hsl(231,77%,90%)',
+        },
       },
     },
   },
   plugins: [],
-}
+};
