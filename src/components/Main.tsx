40491ddeb75bef33e7eefa9c0861a6b02134d2cb
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-26 13:36:20 +0530

    Add Main Component

---
+++ src/components/Main.tsx
@@ -0,0 +1,31 @@
+import { CSSProperties, ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+  bg: {
+    desktop: string;
+    tablet: string;
+    mobile: string;
+  };
+}
+
+export default function Main({
+  children,
+  bg: { desktop, tablet, mobile },
+}: Props) {
+  const styles = {
+    '--bg-desktop': `url(${desktop})`,
+    '--bg-tablet': `url(${tablet})`,
+    '--bg-mobile': `url(${mobile})`,
+  } as CSSProperties;
+
+  return (
+    <main className="grid">
+      <div
+        style={styles}
+        className={`fixed inset-0 w-full h-full -z-10 bg-no-repeat bg-cover bg-[image:var(--bg-mobile)] md:bg-[image:var(--bg-tablet)] lg:bg-[image:var(--bg-desktop)]`}
+      ></div>
+      {children}
+    </main>
+  );
+}
